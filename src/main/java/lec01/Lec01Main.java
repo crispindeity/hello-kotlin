961c357a554ee3f141628f0a79a1d812a3272a20
crispindeity <shoy1415@gmail.com>
2023-09-04 12:14:16 +0900

    코틀린에서 변수를 다루는 방법

---
+++ src/main/java/lec01/Lec01Main.java
@@ -0,0 +1,13 @@
+package lec01;
+
+public class Lec01Main {
+
+    public static void main(String[] args) {
+        long number1 = 10L;
+        final long number2 = 10L;
+
+        Long number3 = 1000L;
+
+        Person person = new Person("Crispin");
+    }
+}
---
+++ src/main/kotlin/lec01/Lec01Main.kt
@@ -0,0 +1,10 @@
+package lec01
+
+fun main() {
+    var number1 = 10L
+
+    var number3: Long? = 1_000L
+    number3 = null
+
+    val person: Person = Person("Crispin")
+}
