87ea830a879d2bed7cabb3044b3cab1902d97a67
crispindeity <shoy1415@gmail.com>
2023-09-14 12:12:11 +0900

    :sparkles: 코틀린에서 중첩 클래스를 다루는 방법

---
+++ src/main/kotlin/lec13/Lec13Main.kt
@@ -0,0 +1,14 @@
+package lec13
+
+fun main() {
+
+}
+
+class House(
+    private val address: String,
+    private val livingRoom: LivingRoom
+) {
+    class LivingRoom(
+        private val area: Double
+    )
+}
